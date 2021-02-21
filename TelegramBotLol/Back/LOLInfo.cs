44d7c488714f899ebdc3a905d5aabb375cf955fc
Artemii <my@email>
2021-02-21 08:40:41 +0300

    Добавьте файлы проекта.

---
+++ TelegramBotLol/Back/LOLInfo.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using RiotSharp;
+using RiotSharp.Endpoints.LeagueEndpoint;
+using RiotSharp.Misc;
+
+
+namespace TelegramBotLol.Back
+{
+
+
+    class LOLInfo
+    {
+        const string apiKey = "RGAPI-397c11da-3b1f-4126-9965-033e4cf7c418";
+        public struct SummonerInfo
+        {
+            public SummonerInfo(SummonerInfo si)
+            {
+                name = si.name;
+                region = si.region;
+                level = si.level;
+                soloWinrate = si.soloWinrate;
+                flexWinrate = si.flexWinrate;
+                soloRank = si.soloRank;
+                flexRank = si.flexRank;
+            }
+            public SummonerInfo(string name)
+            {
+                this.name = name;
+                this.region = name;
+                this.level = name;
+                this.soloWinrate = name;
+                this.flexWinrate = name;
+                this.soloRank = name;
+                this.flexRank = name;
+            }
+            public string name { get; set; }
+            public string region { get; set; }
+            public string level { get; set; }
+            public string soloWinrate { get; set; }
+            public string flexWinrate { get; set; }
+            public string soloRank { get; set; }
+            public string flexRank { get; set; }
+        }
+
+        public List<SummonerInfo> summonerInfos;
+
+        public LOLInfo(string SummonerName, bool quickSearch, Region region = 0)
+        {
+            summonerInfos = new List<SummonerInfo>();
+            if (quickSearch)
+            {
+                for (int i = 0; i < 19; i++)
+                {
+                    summonerInfos.Add(GetName(SummonerName, (Region)i));
+                }
+            }
+            else
+            {
+                summonerInfos.Add(GetInfo(SummonerName, region));
+            }
+        }
+
+        public SummonerInfo GetInfo(string SummonerName, Region region)
+        {
+            SummonerInfo summonerInfo = new SummonerInfo("Unknown");
+
+            string summonername;
+
+            var api = RiotApi.GetDevelopmentInstance(apiKey);
+
+            summonername = SummonerName;
+
+            try
+            {
+                //General info about account
+                var summoner = api.Summoner.GetSummonerByNameAsync(region, summonername).Result;
+                summonerInfo.name = summoner.Name;
+                summonerInfo.region = summoner.Region.ToString();
+                summonerInfo.level = summoner.Level.ToString();
+                var accountid = summoner.AccountId;
+
+                Console.WriteLine("Name:" + summonerInfo.name);
+                Console.WriteLine("Region:" + summonerInfo.region);
+                Console.WriteLine("Level:" + summonerInfo.level);
+
+                try
+                {
+                    List<LeagueEntry> rank = api.League.GetLeagueEntriesBySummonerAsync(summoner.Region, summoner.Id).Result;
+
+                    if (rank[0].QueueType == "RANKED_SOLO_5x5")
+                    {
+                        try
+                        {
+                            //Getting players ranks
+                            Console.WriteLine("Rank solo:" + rank[0].Tier + " " + rank[0].Rank);
+                            summonerInfo.soloRank = rank[0].Tier + " " + rank[0].Rank;
+                            Console.WriteLine("Rank flex:" + rank[1].Tier + " " + rank[1].Rank);
+                            summonerInfo.flexRank = rank[1].Tier + " " + rank[1].Rank;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\nProblems with solorank\n");
+                        }
+
+                        try
+                        {
+                            //Getting players winrates
+                            float sWinrate = (float)rank[0].Wins / ((float)rank[0].Wins + (float)rank[0].Losses);
+                            sWinrate *= 100;
+                            Console.WriteLine("Solo ranked winrate:" + Math.Round(sWinrate, 1) + "%");
+                            summonerInfo.soloWinrate = Math.Round(sWinrate, 1).ToString();
+
+                            float fWinrate = (float)rank[1].Wins / ((float)rank[1].Wins + (float)rank[1].Losses);
+                            fWinrate *= 100;
+                            Console.WriteLine("Flex ranked winrate:" + Math.Round(fWinrate, 1) + "%");
+                            summonerInfo.flexWinrate = Math.Round(fWinrate, 1).ToString();
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\nProblems with winrates\n");
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            //Getting players ranks
+                            Console.WriteLine("Rank flex:" + rank[0].Tier + " " + rank[0].Rank);
+                            summonerInfo.flexRank = rank[0].Tier + " " + rank[0].Rank;
+                            Console.WriteLine("Rank solo:" + rank[1].Tier + " " + rank[1].Rank);
+                            summonerInfo.soloRank = rank[1].Tier + " " + rank[1].Rank;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\nProblems with solorank\n");
+                        }
+
+                        try
+                        {
+                            //Getting players winrates
+                            float fWinrate = (float)rank[0].Wins / ((float)rank[0].Wins + (float)rank[0].Losses);
+                            fWinrate *= 100;
+                            Console.WriteLine("Flex ranked winrate:" + Math.Round(fWinrate, 1) + "%");
+                            summonerInfo.flexWinrate = Math.Round(fWinrate, 1).ToString();
+
+                            float sWinrate = (float)rank[1].Wins / ((float)rank[1].Wins + (float)rank[1].Losses);
+                            sWinrate *= 100;
+                            Console.WriteLine("Solo ranked winrate:" + Math.Round(sWinrate, 1) + "%");
+                            summonerInfo.soloWinrate = Math.Round(sWinrate, 1).ToString();
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\nProblems with winrates\n");
+                        }
+                    }
+                }
+                catch
+                {
+
+                    Console.WriteLine("\nProblems with rank\n");
+                    return summonerInfo;
+                }
+
+                return summonerInfo;
+            }
+            catch
+            {
+                Console.WriteLine("\nPlayer not found (or smth else)\n");
+                return summonerInfo;
+            }
+        }
+
+        public SummonerInfo GetName(string SummonerName, Region region)
+        {
+            SummonerInfo summonerInfo = new SummonerInfo("Unknown");
+
+
+            string summonername;
+
+            var api = RiotApi.GetDevelopmentInstance(apiKey);
+
+            summonername = SummonerName;
+
+            try
+            {
+                var summoner = api.Summoner.GetSummonerByNameAsync(region, summonername).Result;
+                summonerInfo.name = summoner.Name;
+                summonerInfo.region = summoner.Region.ToString();
+                Console.WriteLine($"{summonerInfo.name} has been found in {summonerInfo.region}");
+            }
+            catch
+            {
+                return summonerInfo;
+            }
+            return summonerInfo;
+        }
+    }
+}
---
+++ TelegramBotLol/Front/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using TelegramBotLol.Back;
+using RiotSharp.Misc;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
+
+namespace TelegramBotLol.Front
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+        static void Main()
+        {
+            botClient = new TelegramBotClient("1680617746:AAHzPbbwTnQpUFUoWaVSHwrTyHhyzV2KeCQ");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}, my username is @{me.Username}"
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.OnCallbackQuery += Bot_OnCallbackQuery;
+
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                if (e.Message.Text[0] != '/')
+                {
+                    Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}, an user name is {e.Message.Chat.Username}.");
+
+                    List<Region> rlist = new List<Region>();
+
+                    LOLInfo info = new LOLInfo(e.Message.Text, true);
+
+                    string message = $"{e.Message.Text} has been found in this regions:";
+
+                    bool flagFound = false;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (info.summonerInfos[i].name != "Unknown")
+                        {
+                            rlist.Add((Region)i);
+                            message += (Region)i + " ";
+                            flagFound = true;
+                        }
+                    }
+                    message += "\n Please select region";
+
+                    if (flagFound)
+                    {
+                        var inlineKeybord = new InlineKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData("RU","ru"),
+                            InlineKeyboardButton.WithCallbackData("EUW","euw"),
+                            InlineKeyboardButton.WithCallbackData("EUNE","eune"),
+                            InlineKeyboardButton.WithCallbackData("BR","br"),
+
+                        },
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("NA","na"),
+                            InlineKeyboardButton.WithCallbackData("KR","kr"),
+                            InlineKeyboardButton.WithCallbackData("OCE","oce"),
+                            InlineKeyboardButton.WithCallbackData("TR","tr")
+                        }
+                    }
+                        );
+
+
+                        await botClient.SendTextMessageAsync(
+                         chatId: e.Message.Chat,
+                         text: message,
+                         replyMarkup: inlineKeybord
+                         );
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nSummoner not found\n");
+                        message = " Summoner not found";
+
+                        await botClient.SendTextMessageAsync(
+                         chatId: e.Message.Chat,
+                         text: message
+                         );
+
+                    }
+                }
+
+            }
+        }
+
+        static async void Bot_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
+        {
+            var message = e.CallbackQuery.Message;
+            string summonerName = message.Text.Substring(0, message.Text.IndexOf(' '));
+            string summonerinfo = "";
+
+            switch (e.CallbackQuery.Data)
+            {
+                case "ru":
+                    {
+
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Ru);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "euw":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Euw);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "eune":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Eune);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "br":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Br);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "na":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Na);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "kr":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Kr);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "oce":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Oce);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+
+                case "tr":
+                    {
+                        LOLInfo info = new LOLInfo(summonerName, false, Region.Tr);
+
+                        summonerinfo =
+                            "Nickname:" + info.summonerInfos[0].name +
+                          "\nLevel:" + info.summonerInfos[0].level +
+                          "\nRegion:" + info.summonerInfos[0].region +
+                          "\nSolo Rank:" + info.summonerInfos[0].soloRank +
+                          "\nFlex Rank:" + info.summonerInfos[0].flexRank +
+                          "\nSolo Ranked Winrate:" + info.summonerInfos[0].soloWinrate + "%" +
+                          "\nFlex Ranked Winrate:" + info.summonerInfos[0].flexWinrate + "%";
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            await botClient.EditMessageTextAsync(e.CallbackQuery.Message.Chat.Id.ToString(), e.CallbackQuery.Message.MessageId, summonerinfo);
+
+            await botClient.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
+        }
+    }
+}
